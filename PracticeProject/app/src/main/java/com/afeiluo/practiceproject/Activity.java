ac90b17e7bfbbaa39d8f901e1b4f85cf8b6ae0e3
afeiluo <352172935@qq.com>
2015-09-10 20:13:20 +0800

    sdsd

--- PracticeProject/app/src/main/java/com/afeiluo/practiceproject/Activity.java
+++ PracticeProject/app/src/main/java/com/afeiluo/practiceproject/Activity.java
@@ -20,6 +20,7 @@ public class Activity extends android.app.Activity {
             Toast.makeText(this, "onRestoreInstanceState name:" + name, Toast.LENGTH_SHORT).show();
         }
         hey wozai ceshi
+                sdfsdafsdf
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
         startBtn = (Button) findViewById(R.id.start_btn);
